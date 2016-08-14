02b4193923ce8704d6f54a57b28bf341cc4c67c3
Arthur Kushka <arthurk@wix.com>
2016-08-14 11:44:13 +0300

    Removed press smth to exit functionality.

--- backend/src/main/scala/me/archdev/http/HttpServer.scala
+++ backend/src/main/scala/me/archdev/http/HttpServer.scala
@@ -14,12 +14,8 @@ import scala.io.StdIn
 class HttpServer(route: Route)(implicit actorSystem: ActorSystem, materializer: Materializer) {
 
   def launch(host: String, port: Int) = {
-    val binding = Http().bindAndHandle(route, host, port)
-    println(s"Server is now online at http://$host:$port\nPress RETURN to stop...")
-    StdIn.readLine()
-    import actorSystem.dispatcher
-    binding.flatMap(_.unbind()).onComplete(_ => actorSystem.terminate())
-    println("Server is down...")
+    Http().bindAndHandle(route, host, port)
+    println(s"Server is now online at http://$host:$port")
   }
 
 }
